2a2efd5b8d7636d06c58d0b6414df0a6959e4658
Nicolas Secilio Rodrigues <79922185+NicolasR-S@users.noreply.github.com>
2023-10-11 12:55:26 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,7 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: "localhost",
+    host: "cp2-sqlserver.database.windows.net",
+    port: 1433,
     user: "nicolas",
     password: "Banco@fiap2tds",
     database: "crud"
